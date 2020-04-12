e67aac8c6de05873954f5fbf7912ed39b3bc2ccf
Renan Hartwig <renanhartwig@gmail.com>
2020-04-11 22:52:46 -0300

    Persist request in redis

--- app/factory/scrapingfactory.py
+++ app/factory/scrapingfactory.py
@@ -1,8 +1,13 @@
 from app.handler import tnt
+from app import redis as r
+import json
 
 class ScrapingFactory:
     def scrap(self, data):
         scraping = get_scraping(data['type'])
+        key = data['email']
+        r.rpush(key, json.dumps(data))
+        print(r.lrange(key, 0, -1))
         return scraping.scrap(data)
 
 def get_scraping(type):
