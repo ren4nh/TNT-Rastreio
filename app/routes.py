fda1070279ca75c4751b48df55600fd8adeb1881
Renan Hartwig <renanhartwig@gmail.com>
2020-04-11 21:30:36 -0300

    Update

---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, make_response, jsonify
+from flask_expects_json import expects_json
+import json
+from app import app, factory
+scraping = factory.scrapingfactory.ScrapingFactory()
+
+schema = {
+    'type': 'object',
+    'properties': {
+        'identificacao': {'type': 'string'},
+        'tipodoc': {'type': 'string'},
+        'numdoc': {'type': 'string'},
+        'type': {'type': 'string'},
+        'email': {'type': 'string'}
+    },
+    'required': ['identificacao', 'numdoc', 'tipodoc', 'type', 'email']
+}
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/', methods=['POST'])
+@expects_json(schema)
+def search_order():
+    data = request.json
+    return scraping.scrap(data)
+
+@app.errorhandler(400)
+def bad_request(error):
+    print(error.description)
+    return make_response(jsonify({'error': str(error.description)}), 400)
